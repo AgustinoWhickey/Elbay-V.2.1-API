a91b084e84bedb5d3edb0bd3a56a4ef9060d5220
Agustino Whickey <wakembus98@gmail.com>
2024-12-08 13:32:16 +0700

    Set Registration Auth

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -12,41 +12,38 @@ class Register extends RestController
     {
         parent::__construct();
         $this->load->model("Auth_model");
-        $this->load->model("Company_model");
     }
 
     public function index_post()
 	{
-		$aksi = $this->Auth_model->ceklogin($this->post('email'));
-        if(password_verify( $this->post('password'),$aksi[0]["password"])){
-            if($aksi[0]["is_active"] == 1){
-                $data = [
-                    'id' => $aksi[0]['id'],
-                    'email' => $aksi[0]['email'],
-                    'role' => $aksi[0]['role_id'],
-                    'logged_in' => TRUE
-                ];
-                $this->response( [
-                    'status' => true,
-                    'data' => $data
-                ], RestController::HTTP_OK );
-            } else {
-                $this->response( [
-                    'status' => true,
-                    'data' => 0
-                ], RestController::HTTP_OK );
-            }
+        $aksi = $this->Auth_model->ceklogin($this->post('email',true));
+        if(!$aksi){
+			$data = [
+				'email' => htmlspecialchars($this->post('email',true)),
+				'phone_number' => htmlspecialchars($this->post('phone_number',true)),
+				'username' => htmlspecialchars($this->post('username',true)),
+				'company' => htmlspecialchars($this->post('company',true)),
+				'image' => htmlspecialchars($this->post('image',true)),
+				'password' => htmlspecialchars($this->post('password',true)),
+			];
+
+            $result = $this->Auth_model->insertRegister($data);
+			$this->response( [
+                'status' => true,
+                'data' => $result
+            ], RestController::HTTP_OK );
 		} else {
 			$this->response( [
                 'status' => false,
-                'message' => 'Data not found!'
+                'data' => $aksi
             ], RestController::HTTP_NOT_FOUND );
 		}
+
     }
 
     public function index_get()
 	{
-        $aksi = $this->Auth_model->ceklogin($this->get('email'));
+        $aksi = $this->Auth_model->updateRegister($this->get('token'));
 		if($aksi){
 			$this->response( [
                 'status' => true,
--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -5,9 +5,57 @@ class Auth_model extends CI_Model
 
     public function ceklogin($email)
     {
-        $this->db->select('*');
-        $this->db->where('email', $email);
+        $this->db->select('user.id, user.name as username, user.email, user.password, user.role_id, user.is_active, company.name as company_name, company.logo');
+        $this->db->where('user.email', $email);
+        $this->db->join('company', 'user.id = company.pic_id');
         $aksi = $this->db->get('user')->result_array();
         return $aksi;
+    }
+
+    
+    public function updateRegister($token)
+	{
+        $updated = time();
+
+        $sql = "UPDATE user SET is_active = 1 WHERE token = '$token'";
+
+        $this->db->query($sql);
+
+		return $this->db->affected_rows() == 1;
+	
+	}
+
+    public function insertRegister($data)
+    {
+        $user = [
+            'name' => $data['username'],
+            'email' => $data['email'],
+            'phone_number' => $data['phone_number'],
+            'image' => 'default.jpg',
+            'password' => password_hash($data['password'], PASSWORD_DEFAULT),
+            'role_id' =>  2,
+            'is_active' => 0, 
+            'token' => substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),1,7), 
+            'date_created' => time()
+        ];
+
+        $this->db->insert('user', $user);
+        $pic_id = $this->db->insert_id();
+        if($this->db->affected_rows() == 1){
+            $company = [
+                'pic_id' => $pic_id,
+                'name' => $data['company'],
+                'address' => '',
+                'logo' => $data['image'],
+                'phone_number' =>  '',
+                'created_at' => time()
+            ];
+
+            $this->db->insert('company', $company);
+            return $this->db->affected_rows();
+        } 
+        
+        return 0;
+        
     }
 }
